fdeace8adf29a82573f87dfcb510bc4ff0936d9c
Gefen <gefenbar009@gmail.com>
2023-09-12 16:17:08 +0300

    fixed hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -12,12 +12,13 @@ export default function Hero() {
     const [showSecondLine, setShowSecondLine] = useState(false);
     const [showThirdLine, setShowThirdLine] = useState(false);
     const [showFourthLine, setShowFourthLine] = useState(false);
+    const [showButton, setShowButton] = useState(false);
 
     useEffect(() => {
-        const timer1 = setTimeout(() => setShowFirstLine(true), 50);
+        const timer1 = setTimeout(() => setShowFirstLine(true), 300);
         const timer2 = setTimeout(() => setShowSecondLine(true), text_first.length * 110);
-        const timer3 = setTimeout(() => setShowThirdLine(true), (text_first.length + text_second.length) * 120); // Adjust the delay as needed
-        const timer4 = setTimeout(() => setShowFourthLine(true), (text_first.length + text_second.length + text_third.length) * 122); // Adjust the delay as needed
+        const timer3 = setTimeout(() => setShowThirdLine(true), (text_first.length + text_second.length) * 120);
+        const timer4 = setTimeout(() => setShowFourthLine(true), (text_first.length + text_second.length + text_third.length) * 121);
 
         return () => {
             clearTimeout(timer1);
@@ -27,52 +28,43 @@ export default function Hero() {
         };
     }, []);
 
+    useEffect(() => {
+        const buttonTimer = setTimeout(() => setShowButton(true), 13500);
+
+        return () => {
+            clearTimeout(buttonTimer);
+        };
+    }, []);
+
     return (
         <section id="hero_section" className="hidden">
             <div id="hero">
                 <h1 id="h1_on_hero">
                     {showFirstLine && (
-                        <Typewriter
-                            words={[text_first]}
-                            loop={1}
-                            cursor={false}
-                            typeSpeed={50}
-
-                        />
+                        <Typewriter words={[text_first]} loop={1} cursor={false} typeSpeed={50} />
                     )}
                 </h1>
                 <h2 id="h2_on_hero">
                     {showSecondLine && (
-                        <Typewriter
-                            words={[text_second]}
-                            loop={1}
-                            cursor={false}
-                            typeSpeed={100}
-                        />
+                        <Typewriter words={[text_second]} loop={1} cursor={false} typeSpeed={100} />
                     )}
                 </h2>
                 <h3 id="h3_on_hero">
                     {showThirdLine && (
-                        <Typewriter
-                            words={[text_third]}
-                            loop={1}
-                            cursor={false}
-                            typeSpeed={100}
-                        />
+                        <Typewriter words={[text_third]} loop={1} cursor={false} typeSpeed={100} />
                     )}
                 </h3>
                 <h4 id="h4_on_hero">
                     {showFourthLine && (
-                        <Typewriter
-                            words={[text_fourth]}
-                            loop={1}
-                            cursor={false}
-                            typeSpeed={100}
-                        />
+                        <Typewriter words={[text_fourth]} loop={1} cursor={false} typeSpeed={100} />
                     )}
                 </h4>
                 <br />
-                <a href="files/Gefen_Bar_Resume.pdf" id="button_on_hero" target="_blank" download="gefen_bar_resume">Download Resume</a>
+                {showButton && (
+                    <a href="files/Gefen_Bar_Resume.pdf" id="button_on_hero" target="_blank" download="gefen_bar_resume">
+                        Download Resume
+                    </a>
+                )}
             </div>
         </section>
     );
