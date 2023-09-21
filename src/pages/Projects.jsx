d38558476ac5ec831256010f1eabbd29fbd31d9a
Gefen <gefenbar009@gmail.com>
2023-09-22 00:35:46 +0300

    new design

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,50 +1,49 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import '../styles/Projects.css';
-import AnimatedPageWrapper from '../Components/AnimatedPageWrapper';
 
 const projectsData = [
-  {
-    name: 'Final Year Project',
-    url: 'https://github.com/gefenbar/Website-Eye-Robot-full-version',
-    image: 'orangeTexture.jpg', // Replace with actual image URL
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget.',
-  },
-  {
-    name: 'Unity Game',
-    url: 'https://github.com/gefenbar/Prototype5',
-    image: 'pinkTexture.jpg', // Replace with actual image URL
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget.',
-  },
-  {
-    name: 'More on my GitHub account..',
-    url: 'https://github.com/gefenbar/',
-    image: 'paperTexture.jpg', // Replace with actual image URL
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget.',
-  },
+    {
+        name: 'Final Year Project',
+        url: 'https://github.com/gefenbar/Website-Eye-Robot-full-version',
+        image: 'orangeTexture.jpg', // Replace with actual image URL
+        description: '',
+    },
+    {
+        name: 'Unity Game',
+        url: 'https://github.com/gefenbar/Prototype5',
+        image: 'pinkTexture.jpg', // Replace with actual image URL
+        description: '',
+    },
+    {
+        name: 'More on my GitHub account..',
+        url: 'https://github.com/gefenbar/',
+        image: 'paperTexture.jpg', // Replace with actual image URL
+        description: '',
+    },
 ];
 
 export default function Project() {
-  return (
-    <motion.div initial={{ y: '100%' }} animate={{ y: 0 }} transition={{ duration: 1 }}>
-      <section id="projects_section" className="hidden">
-        <h1>Projects</h1>
-        <div className="projects">
-          {projectsData.map((project, index) => (
-            <div className="project-card" key={index}>
-              <a href={project.url} target="_blank" rel="noopener noreferrer">
-                <div className="project-image">
-                  <img src={project.image} alt={project.name} />
+    return (
+        <motion.div initial={{ y: '100%' }} animate={{ y: 0 }} transition={{ duration: 1 }}>
+            <section id="projects_section" className="hidden">
+                <h1>Projects</h1>
+                <div className="projects">
+                    {projectsData.map((project, index) => (
+                        <div className="project-card" key={index}>
+                            <a href={project.url} target="_blank" rel="noopener noreferrer">
+                                <div className="project-image">
+                                    <img src={project.image} alt={project.name} />
+                                </div>
+                                <div className="project-info">
+                                    <h2>{project.name}</h2>
+                                    <p>{project.description}</p>
+                                </div>
+                            </a>
+                        </div>
+                    ))}
                 </div>
-                <div className="project-info">
-                  <h2>{project.name}</h2>
-                  <p>{project.description}</p>
-                </div>
-              </a>
-            </div>
-          ))}
-        </div>
-      </section>
-    </motion.div>
-  );
+            </section>
+        </motion.div>
+    );
 }
