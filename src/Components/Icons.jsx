afa8324369839fe20fc2c88c72cc1dd525dd332e
Gefen <gefenbar009@gmail.com>
2023-09-25 23:33:14 +0300

    some design fixes

--- src/Components/Icons.jsx
+++ src/Components/Icons.jsx
@@ -1,35 +1,57 @@
 import React from 'react';
 import '../styles/Icons.css';
 
-// Update the Icon component to include the text label
-const Icon = ({ logo, title, url }) => {
+const Icon = ({ logo, title, url, info }) => {
     return (
         <div className="icon">
             <a href={url} target="_blank" rel="noopener noreferrer">
                 <img src={logo} alt="Icon" className="logo" />
             </a>
             <span className="icon-title">{title}</span>
+            <div className="info-popup">{info}</div>
         </div>
     );
 };
 
-
-
 export default function Icons() {
     const iconsData = [
-        { logo: 'linkedin-logo.png', title: 'LinkedIn', url: 'https://www.linkedin.com/in/gefen-bar/' },
-        { logo: 'gmail-logo.png', title: 'Gmail', url: 'mailto:gefenbar23@gmail.com' },
-        { logo: 'phone.png', title: 'Phone', url: 'tel:0502001376' },
-        { logo: 'github-logo.png', title: 'GitHub', url: 'https://github.com/gefenbar' },
-
-
+        {
+            logo: 'linkedin-logo.png',
+            title: 'LinkedIn',
+            url: 'https://www.linkedin.com/in/gefen-bar/',
+            info: 'Connect with me on LinkedIn',
+        },
+        {
+            logo: 'gmail-logo.png',
+            title: 'Gmail',
+            url: 'mailto:gefenbar23@gmail.com',
+            info: 'Send me an email at gefenbar23@gmail.com',
+        },
+        {
+            logo: 'phone.png',
+            title: 'Phone',
+            url: 'tel:0502001376',
+            info: 'Call me at 050-200-1376',
+        },
+        {
+            logo: 'github-logo.png',
+            title: 'GitHub',
+            url: 'https://github.com/gefenbar',
+            info: 'Visit my GitHub profile',
+        },
     ];
 
     return (
         <div className="icons-container">
             <div className="icons">
                 {iconsData.map((icon, index) => (
-                    <Icon key={index} logo={icon.logo} title={icon.title} url={icon.url} />
+                    <Icon
+                        key={index}
+                        logo={icon.logo}
+                        title={icon.title}
+                        url={icon.url}
+                        info={icon.info}
+                    />
                 ))}
             </div>
         </div>
