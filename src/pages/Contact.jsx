e901b38aed57282d9301aa70007ff27b7b5ec0fd
Gefen <gefenbar009@gmail.com>
2023-10-04 19:03:14 +0300

    added email redirecting

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import '../styles/Contact.css';
 import PageEffect from '../Components/PageEffect';
+
 export default function Contact() {
     const [formData, setFormData] = useState({ name: '', email: '', message: '' });
 
@@ -9,11 +10,15 @@ export default function Contact() {
         setFormData({ ...formData, [name]: value });
     };
 
-    const handleSubmit = (e) => {
+    const handleEmailLinkClick = (e) => {
         e.preventDefault();
-        console.log('Form data:', formData);
-        // Reset the form fields
-        setFormData({ name: '', email: '', message: '' });
+        const { name, email, message } = formData;
+
+        // Create a mailto URL with pre-filled fields
+        const mailtoURL = `mailto:${encodeURIComponent('recipient@example.com')}?subject=${encodeURIComponent(`Message from ${name}`)}&body=${encodeURIComponent(message)}`;
+
+        // Open the user's default email client
+        window.location.href = mailtoURL;
     };
 
     return (
@@ -23,21 +28,46 @@ export default function Contact() {
                     <img src="mail-logo.webp" alt="Windows Icon" />
                     <h1>Email</h1>
                 </div>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleEmailLinkClick}>
                     <div className="form-group">
                         <label htmlFor="name">Name:</label>
-                        <input type="text" id="name" name="name" placeholder="Enter your name" value={formData.name} onChange={handleInputChange} required />
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            placeholder="Enter your name"
+                            value={formData.name}
+                            onChange={handleInputChange}
+                            required
+                        />
                     </div>
                     <div className="form-group">
                         <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" name="email" placeholder="Enter email address" value={formData.email} onChange={handleInputChange} required />
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Enter email address"
+                            value={formData.email}
+                            onChange={handleInputChange}
+                            required
+                        />
                     </div>
                     <div className="form-group">
                         <label htmlFor="message">Message:</label>
-                        <textarea type="text" id="message" name="message" placeholder='Insert your message' value={formData.message} onChange={handleInputChange} required />
+                        <textarea
+                            type="text"
+                            id="message"
+                            name="message"
+                            placeholder="Insert your message"
+                            value={formData.message}
+                            onChange={handleInputChange}
+                            required
+                        />
                     </div>
                     <button type="submit">Submit</button>
                 </form>
             </div>
-        </PageEffect>);
+        </PageEffect>
+    );
 }
